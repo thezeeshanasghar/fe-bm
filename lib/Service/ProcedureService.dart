f1bd599d3c530a6144dc4475acfddcbb5f365da8
Aneesjrp <aneesahmad17786@gmail.com>
2021-05-19 15:27:04 +0500

    services added

---
+++ lib/Service/ProcedureService.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:baby_doctor/model/Room.dart';
+import 'package:baby_doctor/model/Services.dart';
+import 'package:http/http.dart' as http;
+import '../model/Procedures.dart';
+
+class ProcedureService {
+  Future<List<Procedures>> getProcedures() async {
+    final response = await http
+        .get(Uri.https('babymedics.fernflowers.com', 'api/procedure'));
+    if (response.statusCode == 200) {
+      log(response.statusCode);
+      List<dynamic> body = jsonDecode(response.body);
+
+      List<Procedures> posts = body
+          .map(
+            (dynamic item) => Procedures.fromJson(item),
+          )
+          .toList();
+      return posts;
+    } else {
+      throw Exception('Failed to load Procedure');
+    }
+  }
+
+  Future<bool> InsertProcedure(Procedures procedures) async {
+    Map<String, dynamic> Obj = {
+      'name': procedures.name,
+      'performedBy': procedures.performedBy,
+      'charges': procedures.charges,
+      'performerShare': procedures.performerShare
+    };
+
+    final response = await http.post(
+        Uri.https('babymedics.fernflowers.com', 'api/procedure'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(Obj));
+
+    if (response.statusCode == 201) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+      return true;
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      return false;
+    }
+  }
+
+  Future<http.Response> UpdateProcedure(Procedures procedures) {
+    return http.put(
+      Uri.https('babymedics.fernflowers.com', 'api/procedure'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(procedures),
+    );
+  }
+
+  Future<http.Response> DeleteProcedure(Procedures procedures) {
+    return http.delete(
+      Uri.https('babymedics.fernflowers.com', 'api/procedure'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(procedures),
+    );
+  }
+}
---
+++ lib/Service/Service.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+import 'package:baby_doctor/model/Services.dart';
+import 'package:http/http.dart' as http;
+
+class Service {
+  Future<List<Services>> getServices() async {
+    final response =
+        await http.get(Uri.https('babymedics.fernflowers.com', 'api/service'));
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body);
+      List<Services> services = body
+          .map(
+            (dynamic item) => Services.fromJson(item),
+          )
+          .toList();
+
+      return services;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load Procedure');
+    }
+  }
+
+  Future<List<Services>> getServiceById(int Id) async {
+    final response = await http
+        .get(Uri.https('babymedics.fernflowers.com', 'api/room/${Id}'));
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body);
+      List<Services> services = body
+          .map(
+            (dynamic item) => Services.fromJson(item),
+          )
+          .toList();
+
+      return services;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load Procedure');
+    }
+  }
+
+  Future<http.Response> InsertServices(Services services) {
+    return http.post(
+      Uri.https('babymedics.fernflowers.com', 'api/room'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(Services),
+    );
+  }
+
+  Future<http.Response> UpdateServices(Services services) {
+    return http.put(
+      Uri.https('babymedics.fernflowers.com', 'api/room/${services.id}'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(services),
+    );
+  }
+
+  Future<http.Response> DeleteServices(Services services) {
+    return http.delete(
+      Uri.https('babymedics.fernflowers.com', 'api/room/${services.id}'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(services),
+    );
+  }
+}
